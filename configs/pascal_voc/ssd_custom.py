7cc5a389de96af1baf400e42ef5140aaa7bfcbd4
Yamahitsuji <goat06131004@gmail.com>
2021-06-10 13:27:23 +0900

    fix code to annotate my data

---
+++ configs/pascal_voc/ssd_custom.py
@@ -0,0 +1,19 @@
+_base_ = ['./ssd300_voc0712.py']
+data_root = 'data/VOCdevkit/'
+data = dict(
+    train=dict(
+        times=3,
+        dataset=dict(
+            ann_file=[data_root + 'VOC2007/ImageSets/Main/train.txt'],
+            img_prefix=[data_root + 'VOC2007/'],
+        ),
+    ),
+    val=dict(
+        ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
+        img_prefix=data_root + 'VOC2007/',
+    ),
+    test=dict(
+        ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
+        img_prefix=data_root + 'VOC2007/',
+    )
+)
